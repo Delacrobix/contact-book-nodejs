03f49f8e439544f4508a72e3b754fa580238c13b
Jeffrey <97353049+Delacrobix@users.noreply.github.com>
2023-05-20 13:07:38 -0500

    adding cors

--- backend/index.js
+++ backend/index.js
@@ -2,7 +2,6 @@ import express from 'express';
 import dotenv from 'dotenv';
 import cors from 'cors';
 import { createHandler } from 'graphql-http/lib/use/express';
-import { createServer as createHttpServer } from 'http';
 import '@babel/register';
 
 dotenv.config({ path: '.env.local' });
@@ -10,6 +9,7 @@ export const app = express();
 
 //HTTP SERVER CONFIG
 const PORT = process.env.PORT;
+const CORS_1 = process.env.CORS_1;
 
 app.listen({ port: PORT }, () => {
   console.log('Server listening on port ' + PORT);
@@ -19,21 +19,11 @@ app.listen({ port: PORT }, () => {
 const { schema } = require('./src/graphql/schema');
 
 const corsOptions = {
-  origin: [
-    'http://contact-book-nodejs.surge.sh/',
-    'https://contact-book-nodejs.surge.sh/',
-    'http://delacrobix.github.io/contact-book-nodejs/',
-    'https://delacrobix.github.io/contact-book-nodejs/',
-    'https://delacrobix.github.io/',
-    'http://delacrobix.github.io/',
-    'http://localhost:3000',
-  ],
+  origin: [CORS_1],
   methods: 'POST',
-  optionsSuccessStatus: 200,
-  exposedHeaders: 'Access-Control-Allow-Origin',
 };
 
-app.use(cors());
+app.use(cors(corsOptions));
 app.use(
   '/graphql',
   createHandler({
